ce5238afda2b9be97dc6546caf438387dce3c229
Kush Daga <kushdaga@hotmail.com>
2021-06-17 19:18:25 +0530

    Working basics

--- src/index.ts
+++ src/index.ts
@@ -12,19 +12,19 @@ const notion = new Client({
 // 	console.log(JSON.stringify(listUsersResponse, null, 2));
 // })();
 
-async function main() {
+async function addToNotion(message: string) {
 	const pageId = "21f68408d92147ee80254eeecc2ae46f";
-	const title = "This is a long ...";
-	const message =
-		"This is a long message from whatsapp that im sending here that has a link https://google.com";
-	const myPage = await notion.pages.retrieve({
-		page_id: pageId,
-	});
+	const title = message.substr(0, 20) + "...";
+	// const message =
+	// 	"This is a long message from whatsapp that im sending here that has a link https://google.com";
+	// const myPage = await notion.pages.retrieve({
+	// 	page_id: pageId,
+	// });
 
-	const blocksInPage = await notion.blocks.children.list({
-		block_id: pageId,
-		page_size: 100,
-	});
+	// const blocksInPage = await notion.blocks.children.list({
+	// 	block_id: pageId,
+	// 	page_size: 100,
+	// });
 
 	// const myDatabases = await notion.search({
 	// 	query: "Programs",
@@ -69,7 +69,42 @@ async function main() {
 		],
 	});
 
-	console.log("Blocks in My Page", JSON.stringify(resp, null, 2));
+	console.log("Added to notion", JSON.stringify(resp, null, 2));
 }
 
-main();
+// main();
+
+// async function twilioConnect() {
+// 	const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// 	const authToken = process.env.TWILIO_AUTH_TOKEN;
+// 	const twilioClient = require("twilio")(accountSid, authToken);
+
+// 	// console.log(twilioClient);
+// }
+
+// twilioConnect();
+
+import http from "http";
+import express from "express";
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
+
+const app = express();
+app.use(express.json());
+app.use(
+	express.urlencoded({
+		extended: false,
+	})
+);
+
+app.post("/sms", async (req, res) => {
+	const twiml = new MessagingResponse();
+	console.log(req.body);
+
+	if (req.body.Body) {
+		await addToNotion(req.body.Body);
+	}
+});
+
+http.createServer(app).listen(1337, () => {
+	console.log("Express server listening on port 1337");
+});
